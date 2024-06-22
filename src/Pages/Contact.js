c9b49bf926ef4164768b681a4a4efa2d1d0e5b32
stere8 <twizoraph4247@gmail.com>
2024-06-22 16:01:35 +0200

    Buildoing up

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import '../Styles/Contact.css'
+import emailjs from 'emailjs-com';
+import '../Styles/Contact.css';
 
 const Contact = () => {
   const { t } = useTranslation();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.send(
+      'service_njpumpf', // Replace with your EmailJS service ID
+      'template_cmtkw4k', // Replace with your EmailJS template ID
+      formData,
+      'jbwNUbuZVBETFQtqM' // Replace with your EmailJS user ID
+    )
+    .then((response) => {
+      console.log('Email sent successfully!', response.status, response.text);
+      alert('Email sent successfully!');
+    })
+    .catch((error) => {
+      console.error('Failed to send email:', error);
+      alert('Failed to send email.');
+    });
+  };
 
   return (
     <div className="container hero-section-bis">
@@ -11,18 +44,18 @@ const Contact = () => {
         <div className="col-md-12">
           <h2>{t('LetsConnectHeading')}</h2>
           <p>{t('ContactFormDescription')}</p>
-          <form method="post">
+          <form onSubmit={handleSubmit}>
             <div>
               <label htmlFor="name">{t('YourNameLabel')}</label>
-              <input id="name" name="name" type="text" />
+              <input id="name" name="name" type="text" value={formData.name} onChange={handleChange} />
             </div>
             <div>
               <label htmlFor="email">{t('EmailLabel')}</label>
-              <input id="email" name="email" type="email" />
+              <input id="email" name="email" type="email" value={formData.email} onChange={handleChange} />
             </div>
             <div>
               <label htmlFor="message">{t('MessageLabel')}</label>
-              <textarea id="message" name="message"></textarea>
+              <textarea id="message" name="message" value={formData.message} onChange={handleChange}></textarea>
             </div>
             <button type="submit">{t('SendButton')}</button>
           </form>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './i18n';
+import emailjs from 'emailjs-com';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+emailjs.init('jbwNUbuZVBETFQtqM'); // Replace with your EmailJS user ID
 
 root.render(
   <React.StrictMode>
