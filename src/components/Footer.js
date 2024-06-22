db5fb94d2d09475d55ec8f775738f1dad03650b0
stere8 <twizoraph4247@gmail.com>
2024-06-22 19:23:47 +0200

    Buildoing up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import '../Styles/Footer.css';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Footer = () => (
   <footer className="border-top footer text-muted">
     <div className="container">
-      &copy; 2024 - OCP - <Link to="/privacy">Privacy</Link>
+      &copy; 2024 - OCP
+      <div className="social-links">
+        <a href="https://github.com/stere8/" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://www.linkedin.com/in/twizeyimana-o-2302a410a" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="tel:+48572170919">Tel: +48 572 170 919</a>
+      </div>
     </div>
   </footer>
 );
