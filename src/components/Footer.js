d96f1117f1d325142f6b178d7778af99cde178fb
stere8 <twizoraph4247@gmail.com>
2025-01-05 21:20:44 +0100

    Update footer styles and content for 2025
    - Added style for `footer h8` in `Footer.css`.
    - Updated footer in `Footer.js` with new company name "Oracle Consults" and changed year to 2025.
    - Introduced new WhatsApp and email links, enhancing the social links section.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import '../Styles/Footer.css';
-import { Link } from "react-router-dom";
 import '@fortawesome/fontawesome-free/css/all.min.css'; // Import Font Awesome CSS
 
 const Footer = () => (
-  <footer className="border-top footer text-muted">
-    <div className="container">
-      &copy; 2024 - OCP - <Link to="/privacy">Privacy</Link>
-      <div className="social-links">
-        <a href="https://github.com/stere8/" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-github"></i> GitHub
-        </a>
-        <a href="https://www.linkedin.com/in/twizeyimana-o-2302a410a" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-linkedin"></i> LinkedIn
-        </a>
-        <a href="tel:+48572170919">
-          <i className="fas fa-phone"></i> Tel: +48 572 170 919
-        </a>
-      </div>
-    </div>
-  </footer>
+    <footer className="border-top footer text-muted">
+        <div className="container">
+            <h8>&copy; 2025 - Oracle Consults</h8>
+            <div className="social-links">
+                <a href="https://github.com/stere8/" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-github"></i> GitHub
+                </a>
+                <a href="https://www.linkedin.com/in/twizeyimana-o-2302a410a" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-linkedin"></i> LinkedIn
+                </a>
+                <a href="tel:+48572170919">
+                    <i className="fas fa-phone"></i> Tel: +48 572 170 919
+                </a>
+                <a href="https://wa.link/p99bkg" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-whatsapp"></i> WhatsApp</a>
+                <a href="mailto:oreste.twizeyimana99@gmail.com"><i className="fas fa-envelope">
+                    </i> Email</a>
+            </div>
+        </div>
+    </footer>
 );
 
 export default Footer;
