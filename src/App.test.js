cb8dcae758afe53a80f7319296c7c2c1dd965f52
stere8 <twizoraph4247@gmail.com>
2026-04-29 15:38:09 +0200

    fix dark mode contrast

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
+import { HelmetProvider } from 'react-helmet-async';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import './i18n';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('react-ga4', () => ({
+  initialize: jest.fn(),
+  send: jest.fn()
+}));
+
+test('renders the application shell', async () => {
+  render(
+    <HelmetProvider>
+      <MemoryRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <App />
+      </MemoryRouter>
+    </HelmetProvider>
+  );
+
+  expect(await screen.findByRole('heading', { level: 1 })).toBeInTheDocument();
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,40 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false
+  })
+});
+
+Object.defineProperty(window, 'scrollTo', {
+  writable: true,
+  value: () => {}
+});
+
+class IntersectionObserverMock {
+  observe() {}
+
+  unobserve() {}
+
+  disconnect() {}
+}
+
+Object.defineProperty(window, 'IntersectionObserver', {
+  writable: true,
+  value: IntersectionObserverMock
+});
+
+Object.defineProperty(global, 'IntersectionObserver', {
+  writable: true,
+  value: IntersectionObserverMock
+});
